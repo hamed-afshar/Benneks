fdc45ab2684150dcdd19067a589e5c75362a5fc1
hamed-afshar <hamed.afshar.n@gmail.com>
2016-12-07 01:58:40 +0330

    In process
    1- Search box added to admin.php
    2- Search.php added for search reason
    3- need some modification to work well

---
+++ search.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * Script to search values in db
+ * 
+ */
+ob_start();
+session_start();
+require 'src/benneks.php';
+// if session is not set this will redirect to login page
+if (!isset($_SESSION['user'])) {
+    header("Location: index.php");
+    exit();
+}
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+if (isset($_POST['searchButton'])) {
+    $searchOption = $_POST['searchOption'];
+    $searchValue = $_POST['searchInput'];
+    $searchquery = "";
+    switch ($searchOption) {
+        case "code":
+            $searchQuery = "WHERE orders.orderID = " . $searchValue . "";  
+            break;
+        case "name":
+            $searchQuery = "WHERE users.userName = " . $searchValue . "";
+            break;
+        case "done":
+            $searchQuery = "WHERE stat.orderStatus = انجام شده";
+            break;
+        case "cancel":
+            $searchQuery = "WHERE stat.orderStatus = لغو";
+            break;
+        case "unknown":
+            $searchQuery = "WHERE stat.orderStatus IS NULL";
+            break;
+        default :
+            $searchQuery = "";
+            break;
+    }
+    $_SESSION['searchQuery'] = $searchQuery;
+    header("Location: admin.php");
+}
+?>

68c5cbf527f5ef475c0038ead4480b0de9c7455e
hamed-afshar <hamed.afshar.n@gmail.com>
2017-10-14 01:13:08 +0330

    composer added for programming

--- benneksBot.php
+++ benneksBot.php
@@ -1,22 +1,41 @@
 <?php
-$benneksBotToken = "463292906:AAFXW6XGDdA3jZqFkBwKjTH8qWBbIl_w0vs";
-$telegramURL = "https://api.telegram.org/bot".$benneksBotToken."/";
 
-//read incoming info and grab the chatID
+define('API_KEY', '463292906:AAFXW6XGDdA3jZqFkBwKjTH8qWBbIl_w0vs');
+$keyboard = ['a', 'b'];
+
+function bot($method, $data = []) {
+    $url = "https://api.telegram.org/bot" . API_KEY . "/" . $method;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    $res = curl_exec($ch);
+    file_put_contents('ch.txt', $data);
+    if (curl_error($ch)) {
+        var_dump(curl_error($ch));
+    } else {
+        return json_decode($res);
+    }
+}
+
 $content = file_get_contents("php://input");
+//fetch update
 $update = json_decode($content, TRUE);
 $message = $update["message"];
 $chatID = $message["chat"]["id"];
-//file_put_contents('log.txt', $chatID);
-
-
-//compose reply
-$reply = "hello everyone!";
-
-//send reply
-$sendto = $telegramURL."sendmessage?chat_id=".$chatID."&text=".$reply;
-file_get_contents($sendto);
-file_put_contents('log.txt', $sendto);
-echo $sendto;
+$text = $message["text"];
+//statement begining
+if ($text == "/start") {
+    //bot('sendMessage', ['chat_id' => $chatID, 'text' => "hello hamedi"]);
+    /*$reply_markup = replyKeyboardMarkup([
+        'keyboard' => $keyboard,
+        'resize_keyboard' => TRUE,
+        'one_time_keyboard' => TRUE
+    ]);*/
+    bot('sendMessage', ['chat_id' => $chatID, 'text' => "hello hamedi", 'replyKeyboardMarkup', 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => true]);
+    //$txt = json_encode(array(chat_id => $chatID, text => "hello hamedi"));
+    //file_put_contents('log.txt', $txt);
+    //bot('sendMessage', $txt);
+}
 ?>
 

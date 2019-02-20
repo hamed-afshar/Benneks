bf168f22dd861c215dc0ec337fb21cf673fe583f
hamed-afshar <hamed.afshar.n@gmail.com>
2019-02-20 16:32:20 +0330

    Change Tl from 2490 to 2700

--- Javascripts/script.js
+++ Javascripts/script.js
@@ -113,7 +113,7 @@ function calculator(userID, country, clothesType, productPrice) {
         case "ترکیه":
             var country = "Turkey";
             var currency = "TL";
-            var currencyRate = 2490;
+            var currencyRate = 2700;
             var weightCost = 60000; //57000 per killo
             var shippingCost = 40000; //defualt for shoes
             break;
